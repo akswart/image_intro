32d786f96d6faceac4728ecee4df34fab334fac0
akswart <akswart@gmail.com>
2017-12-07 03:40:29 -0600

    35

--- src/src/test1.java
+++ src/src/test1.java
@@ -1,2 +1,18 @@
 public class test1 {
+
+  /*
+
+  This is a multiple line comment
+   */
+
+  public static void main(String args[]) {
+    double gallons, liters;
+
+    gallons = 10;
+
+    liters = gallons * 3.7854;
+
+    System.out.println(gallons + " gallons is " + liters + " many liters");
+  }
+
 }
